3039f0688c92bdf3dffef28ec3d6e5d8e7f4acce
Ishwant Singh <ishwant8@gmail.com>
2019-03-15 17:57:59 +0530

    feat: clicking '+' increases 'count' and '-' decreases 'count'

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
+import { type } from "os";
+import { bindActionCreators } from "redux";
+
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
 
 // Our action creators will return
 // an action packet that our reducer will
@@ -9,10 +12,22 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
-export const increment = () => {
-  // Fill in this function
-};
+export const increment = () => ({
+  type: INCREMENT
+});
 
-export const decrement = () => {
-  // Fill in this function
-};
+export const decrement = () => ({
+  type: DECREMENT
+});
+
+//can use this too but is longer :->
+
+// export function mapDispatchToProps(dispatch) {
+//   return bindActionCreators(
+//     {
+//       increment,
+//       decrement
+//     },
+//     dispatch
+//   );
+// }
